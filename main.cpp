f6284f467054efb49d5754a186515023a52d2f2b
Lakeev Gergiy <lakeev.gosha2016@yandex.ru>
2022-09-11 21:27:37 +0300

    The Prototype of the programm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+class Car{
+ 
+}
+
 int main(){
-  cout << "Hello world!";
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
   return 0;
  }
